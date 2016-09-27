fb1c7c44082f210aaffd13f3d81ba51f33bf8582
Joel Sene <joelsene@joels-mac.local>
2016-09-27 17:36:10 -0300

    Initial Commit

---
+++ contacts001/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  contacts001
+//
+//  Created by Joel Sene on 27/09/16.
+//  Copyright © 2016 Barba Ruiva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ contacts001/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  contacts001
+//
+//  Created by Joel Sene on 27/09/16.
+//  Copyright © 2016 Barba Ruiva. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
